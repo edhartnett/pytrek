f72c51b177726ae2242efa712e89219338028910
Ed <edward.hartnett@noaa.gov>
2024-10-04 07:56:54 -0600

    more documentation

--- src/pytrek/enterprise.py
+++ src/pytrek/enterprise.py
@@ -3,13 +3,12 @@ The Starship Engerprise: NCC-1701
 """
 
 def fire_phaser(setting):
-    """
+    '''
     Fire phasers!
 
     :param setting: "stun" or "kill"
 
     :return: Whether or not we hit.
     :rtype: integer
-    """
-    
+    '''
     print("Fire phaser setting: " + setting)
