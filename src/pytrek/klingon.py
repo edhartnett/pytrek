4c2cf99d47263af2d75df21640f3633da7466864
Ed <edward.hartnett@noaa.gov>
2024-10-04 13:28:04 -0600

    added klingon

---
+++ src/pytrek/klingon.py
@@ -0,0 +1,16 @@
+"""
+Recorded history begins with the story of the creation of Kortar
+and his mate, who were recognized as the first Klingons.
+
+"""
+
+def revenge(temperature):
+    '''
+    Get revenge.
+
+    :param temperature: Best served cold.
+
+    :return: Whether or not we got revenge.
+    :rtype: integer
+    '''
+    print("Revenge is a dish best served " + temperature)
