6c415d631646c50fcc1b0fae55fc1b9327788c7c
Edward Hartnett <edward.hartnett@noaa.gov>
2024-10-05 07:21:01 -0600

    fixed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'numpydoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
     'sphinx.ext.inheritance_diagram']
