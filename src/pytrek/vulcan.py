714350af5fa30a10e833c03cfe7655f4c41ac11d
Edward Hartnett <edward.hartnett@noaa.gov>
2024-10-05 07:43:12 -0600

    added vulcans

---
+++ src/pytrek/vulcan.py
@@ -0,0 +1,14 @@
+"""
+Vulcans are noted for their strict adherence to logic and reason and
+suppression of emotion.
+
+"""
+
+def neck_pinch():
+    '''
+    Pinch neck to save Kirk.
+
+    :return: Whether or not neck was successfully pinched.
+    :rtype: integer
+    '''
+    print("Ouch, my neck!")
